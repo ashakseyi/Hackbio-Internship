93e2c085bd8bf2918927eabcd877f1d95488186f
ashakseyi <ashakseyi@gmail.com>
2025-08-30 16:16:57 +0100

    Update Linux and Bash Fundamentals.sh

--- Linux and Bash Fundamentals.sh
+++ Linux and Bash Fundamentals.sh
@@ -16,37 +16,45 @@ mkdir -p ~/Oluwaseyi_Ashaka
 # Create another new directory titled biocomputing and change to that directory
 mkdir -p ~/biocomputing && cd ~/biocomputing || exit
 
-# Download files
+# Download these 3 files:
+#https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+#https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+#https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+
 wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
 wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
 
 # Move the .fna file to the folder titled your name
 mv wildtype.fna ~/Oluwaseyi_Ashaka/
 
-# Delete duplicate gbk file if it exists
+# Delete duplicate gbk file
 rm -f wildtype.gbk.1
 
 # Confirm if the .fna file is mutant or wild type (tatatata vs tata)
 grep "tatatata" wildtype.fna
 grep "tata" wildtype.fna
 
-# If mutant, print all matching lines into a new file
+# If mutant, print all matching lines into a new file (mutant_1.txt)
 grep "tatatata" wildtype.fna > mutant_1.txt
 
-# Count number of lines (excluding header) in the .gbk file
+# Count number of lines (excluding header) in the .gbk file 
 grep -v LOCUS wildtype.gbk | wc -l
 
-# Print the sequence length of the .gbk file
+# Print the sequence length of the .gbk file (Use the LOCUS tag in the first line)
 grep "LOCUS" wildtype.gbk
 
-# Print the source organism of the .gbk file
+# Print the source organism of the .gbk file (Use the SOURCE tag in the first line) 
 grep "SOURCE" wildtype.gbk
 
-# List all the gene names of the .gbk file
+# List all the gene names of the .gbk file Hint {grep '/gene='}
 grep "/gene=" wildtype.gbk
 
-# List contents of both directories
-ls ~/biocomputing ~/Oluwaseyi_Ashaka
+#Clear your terminal space and print all commands used today
+clear
+history
+ls biocomputing/ Oluwaseyi_Ashaka/
+
 
 
 #######################################
@@ -63,25 +71,37 @@ conda create -n funtools python=3.10 -y
 # Activate the funtools environment
 conda activate funtools
 
+#Install Figlet using conda or apt-get
 # Add channels
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 # Install Figlet
-conda install -y pyfiglet || sudo apt-get install -y figlet
+conda install -y pyfiglet || 
+#Alternatively, use apt-get
+sudo apt-get install -y figlet
 
-# Run figlet
+# Run figlet <your name>
 figlet "Oluwaseyi Ashaka"
 
 # Install bioinformatics tools
-conda install -y bwa
-conda install -y blast
-conda install -y samtools
-conda install -y bedtools
-conda install -y spades
-conda install -y bcftools
-conda install -y fastp
+#Install bwa through the bioconda channel
+conda install bwa
+#Install blast through the bioconda channel
+conda install blast
+#Install samtools through the bioconda channel
+conda install samtools
+#Install bedtools through the bioconda channel
+conda install bedtools
+#Install spades.py through the bioconda channel
+conda create -n spades_env spades
+ conda install spades
+#Install bcftools through the bioconda channel
+conda install bcftools
+#Install fastp through the bioconda channel
+conda install fastp
 
+#Install multiqc through the bioconda channel
 # Create and activate new environment for multiqc
 conda create -n myenv python=3.11 -y
 conda activate myenv
